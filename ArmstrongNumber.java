e56fa34533961f483d68b46edcc2d9b4d5ede5e6
Arvind Kumar Agrahari <46920111+arvindagrahari0505@users.noreply.github.com>
2019-01-22 17:55:00 +0530

    Add files via upload

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -3,7 +3,20 @@ public class ArmstrongNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		for(int i=1;i<=1000;i++) {
+			int len = Integer.toString(i).length();
+			int temp=i;
+			int sum=0;
+			while(temp!=0) {
+				int m=temp%10;
+				sum+=(int)Math.pow(m, len);
+				temp/=10;
+			}
+			if(sum==i) {
+				System.out.print(i+" ");
+			}
+		}
 	}
 
 }
--- Panagram.java
+++ Panagram.java
@@ -5,8 +5,22 @@ public class Panagram {
 		// TODO Auto-generated method stub
 		
 		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		System.out.println(str);
+		String str = sc.nextLine().toLowerCase().replaceAll("[^a-zA-Z]", "");
+		int[] arr = new int[26];
+		for(int i=0;i<26;i++) {
+			arr[i]=0;
+		}
+		for(int i=0;i<str.length();i++) {
+			int index = (str.charAt(i)-97);
+			arr[index]++;
+		}
+		for(int i=0;i<26;i++) {
+			if(arr[i]==0) {
+				System.out.println("It is not a Panagram");
+				System.exit(0);
+			}
+		}
+		System.out.println("It is a Panagram");
 		sc.close();
 	}
 
